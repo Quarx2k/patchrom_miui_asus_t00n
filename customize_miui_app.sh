72d6b33cbe8d5c1f797631d70c6cdb5a8496b5b6
小煞-Xs <imxs1107@gmail.com>
2015-09-30 11:09:59 +0800

    Update customize_miui_app.sh

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #
-# $1: dir for original miui app
-# $2: dir for target miui app
-#
 
 XMLMERGYTOOL=$PORT_ROOT/tools/ResValuesModify/jar/ResValuesModify
 GIT_APPLY=$PORT_ROOT/tools/git.apply
